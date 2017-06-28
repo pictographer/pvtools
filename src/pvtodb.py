1bcfe1c40314d21b2c54bc4c9d44464e5f5f73a4
Pictographer <corbett@pictographer.com>
2017-06-27 22:24:14 -0700

    Added parsing of production.html. Improved modularity.

--- src/pvtodb.py
+++ src/pvtodb.py
@@ -2,7 +2,9 @@ import sys
 import re
 from lxml import etree
 
-data_file = '../test/data/2017-05-02T06:15-0700/192.168.1.215/home.html'
+path = '../test/data/2017-05-02T06:15-0700/192.168.1.215/'
+home_file = path + 'home.html'
+production_file = path + 'production.html'
 production_label = 'Currently generating'
 version_label = 'Current Software Version'
 checked_version = 'R3.7.28 (88072d)'
@@ -17,17 +19,25 @@ def successor_text(root, label):
     assert(len(head) == 1)
     return head[0].text.strip()
 
-# The input files are short. Read the entire thing.
-raw_html = open(data_file, 'r').read()
-try:
-    root = etree.HTML(raw_html)
-except Exception as e:
-    print(repr(e))
+def parse_html(data_file):
+    '''Read and parse the HTML data_file, returning the lxml parse tree.'''
+    # The input files are short. Read the entire thing.
+    raw_html = open(data_file, 'r').read()
+    try:
+        root = etree.HTML(raw_html)
+        return root
+    except Exception as e:
+        print(repr(e))
 
-observed_version = successor_text(root, version_label)
+home_root = parse_html(home_file)
+observed_version = successor_text(home_root, version_label)
 if observed_version != checked_version:
     print("{}:Warning: version has changed from {} to {}." .
           format(sys.argv[0], checked_version, observed_version))
 
 # Report the power generated.
-print(successor_text(root, production_label))
+print(successor_text(home_root, production_label))
+
+production_root = parse_html(production_file)
+for field_label in ["Currently", "Today", "Past Week"]:
+    print(successor_text(production_root, field_label))
